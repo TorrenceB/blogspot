66fc5b6fc1298a9352c883d4fd71ee012c6fae9e
torrenceb <torrenceb90@gmail.com>
2022-10-04 17:31:07 -0600

    Init tailwind dependencies

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,8 @@
   <div></div>
 </template>
 
-<script>
-export default {
-  name: "App",
-};
+<script setup>
+// import { reactive, computed } from "vue";
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
