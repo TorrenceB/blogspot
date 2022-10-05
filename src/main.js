53e96bbd400e1c39fca00c406abe5e29ba3c38e4
torrenceb <torrenceb90@gmail.com>
2022-10-04 18:17:32 -0600

    Install flowbite

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./styles/app.css";
+import "flowbite";
 
 createApp(App).mount("#app");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const flowbite = require("flowbite/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  purge: [],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite],
 };
